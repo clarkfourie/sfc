11db9b20405805e70b0b6dcae821ca7a8acb18a1
clark <gacfourie@live.com>
2015-11-14 14:20:55 +0200

    dev test 4

--- app/models/sfcList.js
+++ app/models/sfcList.js
@@ -13,3 +13,19 @@ exports.definition = {
         }
     }
 };
+
+exports.definition = {
+    config : {
+        columns : {
+            "id_sfcList": "INTEGER PRIMARY KEY AUTOINCREMENT",
+            "value" : "TEXT",
+            "hasCheck": "BOOLEAN",
+            "isCorrect" : "BOOLEAN"
+        },
+        adapter : {
+            type : "sql",
+            collection_name : "sfcList",
+            idAttribute: "id_sfcList",
+        }
+    }
+};
