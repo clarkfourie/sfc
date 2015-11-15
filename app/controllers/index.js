1d6d9df5fe552a5d08ce3ffad5abf0dfbf663832
clark <gacfourie@live.com>
2015-11-15 12:47:36 +0200

    cleanup before new approach

--- app/controllers/index.js
+++
@@ -1,7 +0,0 @@
-if (OS_IOS) {
-    // If the user is already logged in, show the home view. If
-    // the user is not yet login, show the login view.
-    Alloy.createController(Alloy.Models.user.validateAuth() ? 'home' : 'login').getView().open();
-} else {
-    $.index.open();
-}
