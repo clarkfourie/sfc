2fa01f3819293968409e4b054d3f0bc2cd527342
clark <gacfourie@live.com>
2015-11-16 21:53:22 +0200

    progress, php still an issue

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,22 +1,42 @@
 $.index.open();
 
+var hostUrl = "http://localhost:80/post_auth.php";
+
 // function doClick(e){
     // alert($.tfUsern.value);
 // };
 
-
-var loginReq = Titanium.Network.createHTTPClient();
+var loginReq = Ti.Network.createHTTPClient(
+{
+	onload: function()
+	{
+		alert('onload function');
+	    var json = this.responseText;
+	    var response = JSON.parse(json);
+	    if (response.logged == true)
+	    {
+	        alert("Welcome " + response.name + ". Your email is: " + response.email);
+	    }
+	},
+	onerror: function(e) {
+		// this function is called when an error occurs, including a timeout
+		alert(response.message);
+   }, 
+	timeout: 5000
+});
  
-function submit(e)
+function loginSubmit(e)
 {
     if ($.tfUsern.value != '' && $.tfPassw.value != '')
     {
-        loginReq.open("POST","http://localhost/post_auth.php");
+        loginReq.open("POST", hostUrl);
         var params = {
             username: $.tfUsern.value,
             password: Ti.Utils.md5HexDigest($.tfPassw.value)
         };
+        alert('about to send params' + params.username + params.password);
         loginReq.send(params);
+        alert('after loginReq.send');
     }
     else
     {
@@ -24,16 +44,4 @@ function submit(e)
     }
 };
 
-loginReq.onload = function()
-{
-    var json = this.responseText;
-    var response = JSON.parse(json);
-    if (response.logged == true)
-    {
-        alert("Welcome " + response.name + ". Your email is: " + response.email);
-    }
-    else
-    {
-        alert(response.message);
-    }
-};
+
