43b8777cc8fa7d6d0ce324ae96b09eab90079cba
clark <gacfourie@live.com>
2015-11-16 16:54:53 +0200

    alloy views and working server but problems with php requests

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,52 +1,20 @@
-var win = Titanium.UI.currentWindow;
- 
-var username = Titanium.UI.createTextField({
-    color:'#336699',
-    top:10,
-    left:10,
-    width:300,
-    height:40,
-    hintText:'Username',
-    keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
-    returnKeyType:Titanium.UI.RETURNKEY_DEFAULT,
-    borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
-});
-win.add(username);
- 
-var password = Titanium.UI.createTextField({
-    color:'#336699',
-    top:60,
-    left:10,
-    width:300,
-    height:40,
-    hintText:'Password',
-    passwordMask:true,
-    keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
-    returnKeyType:Titanium.UI.RETURNKEY_DEFAULT,
-    borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
-});
-win.add(password);
- 
-var loginBtn = Titanium.UI.createButton({
-    title:'Login',
-    top:110,
-    width:90,
-    height:35,
-    borderRadius:1,
-    font:{fontFamily:'Arial',fontWeight:'bold',fontSize:14}
-});
-win.add(loginBtn);
+$.index.open();
+
+// function doClick(e){
+    // alert($.tfUsern.value);
+// };
+
 
 var loginReq = Titanium.Network.createHTTPClient();
  
-loginBtn.addEventListener('click',function(e)
+function submit(e)
 {
-    if (username.value != '' && password.value != '')
+    if ($.tfUsern.value != '' && $.tfPassw.value != '')
     {
-        loginReq.open("POST","http://localhost:8888/post_auth.php");
+        loginReq.open("POST","http://localhost/post_auth.php");
         var params = {
-            username: username.value,
-            password: Ti.Utils.md5HexDigest(password.value)
+            username: $.tfUsern.value,
+            password: Ti.Utils.md5HexDigest($.tfPassw.value)
         };
         loginReq.send(params);
     }
@@ -54,7 +22,7 @@ loginBtn.addEventListener('click',function(e)
     {
         alert("Username/Password are required");
     }
-});
+};
 
 loginReq.onload = function()
 {
